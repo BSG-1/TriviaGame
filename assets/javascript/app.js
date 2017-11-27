5e814970608920caa6bbfa0e4df4dad6e3d78d48
Bahadur Ghataorhe <b.ghataorhe@gmail.com>
2017-11-26 17:14:45 -0800

    adds small updates

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,44 +1,51 @@
-//Questions Array
-var gameQuestions = {
-		question1: "Who was Luke's Father?",
-		answers1: {
-			a: "Darth Vader", 
-			b: "Darth Plageuis", 
-			c: "The Force",
-			d: "General Akbar"
-		},
-		correctAnswer1: 'a',
+$(document).ready(function(){
 
-		question2: "Han Solo & Leia Skywalker/Organa/Solo had a son. What was his name?",
-		answers2: {
-			a: "Kylo Ren", 
-			b: "Sheev Paplatine", 
-			c: "Jacen Solo",
-			d: "General Akbar"
-		},
-		correctAnswer2: 'c',	
+var gameQuestions = ["Who was Luke's Father?", "Han Solo & Leia Skywalker/Organa/Solo had a son. What was his name?", 
+					"General Akbar exclaimed which of these?", "Which Sith Lord created the Rule of Two?"];
+//question objects
+/* var q1 = {
+	question: "Who was Luke's Father?",
+	answers: [
+			'a: Darth Vader', 
+			'b: Darth Plagueis',
+			'c: The Force',
+			'd: General Akbar'],
+	correctAnswer: 'a: Darth Vader',
+};
 
-		question3: "General Akbar exclaimed which of these?",
-		answers3: {
-			a: "Run for your lives!", 
-			b: "It's a trap!", 
-			c: "Sucks to suck, huh?",
-			d: "You can't sit with us."
-		},
-		correctAnswer3: 'b',
-
-		question4: "Who was the Rule of Two created by?",
-		answers4: {
-			a: "Darth Bane", 
-			b: "Anakin Skywalker", 
-			c: "Emperor Palpatine",
-			d: "Darth Jar-Jar"
-		},
-		correctAnswer4: 'a',
-	}
+var q2 = {
+	question: "Han Solo & Leia Skywalker/Organa/Solo had a son. What was his name?",
+	answers: [
+			'a: Kylo Ren',
+			'b: Sheev Paplatine',
+			'c: Jacen Solo',
+			'd: General Akbar'],
+	correctAnswer2: 'c: Jacen Solo',
+};
+			
+var q3 = {
+	question: "General Akbar exclaimed which of these?",
+	answers: [
+			'a: Run for your lives!', 
+			'b: Its a trap!', 
+			'c: Sucks to suck, huh?',
+			'd: You cant sit with us.'],
+	correctAnswer: 'b: Its a trap!',
+};
+		
+var q4 = {
+	question4: "Who was the Rule of Two created by?",
+	answers4: [
+			'a: Darth Bane', 
+			'b: Anakin Skywalker', 
+			'c: Emperor Palpatine',
+			'd: Darth Jar-Jar'],
+	correctAnswer4: 'a: Darth Bane',
+};
+*/
 
 //showQuestion will hold the setInterval when we start the game
-var showQuestion;
+var newQuestion;
 
 //questionCount keeps track of the index of the currently displaying question
 var questionCount = 0; 
@@ -47,9 +54,9 @@ var questionCount = 0;
 $("#start").click(startGame);
 
 //function to display every question
-function displayQuestion(){
-	for (var i = 0; i < gameQuestions.question.length; i++) {
-		$("#questions").text(gameQuestions.question);
+function newQuestion(){
+	for (var i = 0; i < gameQuestions.length; i++) {
+		$("#questions").append(gameQuestions.questions[i]);
 	}
 	
 };
@@ -58,7 +65,7 @@ function displayQuestion(){
 
 
 //function to go to the next question
-function nextQuestion(){
+function newQuestion(){
 	questionCount++;
 	
 	//show the next question
@@ -79,4 +86,15 @@ function startGame() {
 }
 
 //run displayQuestion function
-displayQuestion();
+newQuestion();
+
+});
+
+
+
+//This one will make sure that every 20 seconds, your questions get "moved on" to the next question. 
+//You can do the same with an interval, like so:
+setInterval(function(){
+  questionCount++;
+  newQuestion();
+}, 20000);
